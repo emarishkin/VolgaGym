ec1b15a4ed517c09aaaa080527043b624480e2cd
Your Name <ee.marishkin@yandex.ru>
2025-06-23 13:38:02 +0400

    BottomNavigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { HomePage } from "./pages/HomePage"
-import { AddTrainingPage } from "./pages/AddTrainingPage"
-import { TrainingHistoryPage } from "./pages/TrainingHistoryPage"
-import { BottomNavigation } from "./components/BottomNavigation"
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { AddTrainingPage } from "./pages/AddTrainingPage";
+import { TrainingHistoryPage } from "./pages/TrainingHistoryPage";
+import { BottomNavigation } from "./components/BottomNavigation";
+import './styles/global.css'
 
+const AppContent = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
+  return (
+    <div className="app-container">
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add-workout" element={<AddTrainingPage />} />
+          <Route path="/training-history" element={<TrainingHistoryPage />} />
+        </Routes>
+      </main>
+      {!isHomePage && <BottomNavigation />}
+    </div>
+  );
+};
 
 export const App = () => {
- 
   return (
-  <Router>
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/add-workout" element={<AddTrainingPage/>}/>
-        <Route path="/training-history" element={<TrainingHistoryPage/>}/>
-      </Routes>
-    </main>
-    <BottomNavigation />
-  </Router>
-  )
-}
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
 
-export default App
+export default App;
